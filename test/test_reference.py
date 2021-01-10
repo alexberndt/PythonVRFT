9cd81436d40c4fa55999c12a7e0cae83d70b7ae4
Alexander <alberndt@kth.se>
2021-01-10 17:33:13 +0100

    Created difficult test that lfilter does not pass

--- test/test_reference.py
+++ test/test_reference.py
@@ -20,16 +20,16 @@ from vrft.vrft_algo import *
 
 
 class TestReference(TestCase):
-    def test_virtualReference(self):
+    def test_virtual_reference_basic(self):
         # wrong system
         with self.assertRaises(ValueError):
             virtual_reference(1, 1, 0)
 
-        # cant be constant the system
+        # system cannot be a constant 
         with self.assertRaises(ValueError):
             virtual_reference([1],[1], 0)
 
-        # cant be constant the system
+        # system cannot be a constant of (2/3)
         with self.assertRaises(ValueError):
             virtual_reference(np.array(2), np.array(3), 0)
 
@@ -37,6 +37,8 @@ class TestReference(TestCase):
         with self.assertRaises(ValueError):
             virtual_reference([1], [1, 1], 0)
 
+
+    def test_virtual_reference_1_order_system(self):
         t_start = 0
         t_end = 10
         t_step = 1e-2
@@ -53,15 +55,21 @@ class TestReference(TestCase):
         # wrong initial conditions
         with self.assertRaises(ValueError):
             r, _ = virtual_reference(data, num, den)
-    
-        #test good data, first order
+
+        num = [0.1]
+        den = [1, -0.9]
         data = iddata(y,u,t_step,[0])
-
         r, _ = virtual_reference(data, num, den)
-
         for i in range(len(r)):
             self.assertTrue(np.isclose(r[i], u[i]))
 
+
+    def test_virtual_reference_2_order_system(self):
+        t_start = 0
+        t_end = 10
+        t_step = 1e-2
+        t = np.arange(t_start, t_end, t_step)
+        u = np.ones(len(t)).tolist()
 
         num = [1-1.6+0.63]
         den = [1, -1.6, 0.63]
@@ -69,8 +77,50 @@ class TestReference(TestCase):
         t, y = scipysig.dlsim(sys, u, t)
         y = y[:,0]
         data = iddata(y,u,t_step,[0,0])
-        #test second order
         r, _ = virtual_reference(data, num, den)
         for i in range(len(r)):
             self.assertTrue(np.isclose(r[i], u[i]))
+
+     
+    def test_virtual_reference_complex_system_1(self):
+        t_start = 0
+        t_end = 10
+        t_step = 1e-2
+        t = np.arange(t_start, t_end, t_step)
+        u = np.ones(len(t)).tolist()
+
+        omega = 10
+        alpha = np.exp(-t_step*omega)
+        num = [(1-alpha)**2] 
+        den = [1, -2*alpha, alpha**2, 0]
+
+        sys = scipysig.TransferFunction(num, den, dt=t_step)
+        u = np.random.normal(size=len(t))
+        t, y = scipysig.dlsim(sys, u, t)
+        data = iddata(y,u,t_step,[0,0,0])
+        r, _ = virtual_reference(data, num, den)
+        for i in range(len(r)):
+            self.assertTrue(np.isclose(r[i], u[i]))
+
+
+    def test_virtual_reference_complex_system_2(self):
+        t_start = 0
+        t_end = 10
+        t_step = 1e-2
+        t = np.arange(t_start, t_end, t_step)
+        u = np.ones(len(t)).tolist()
+
+        num = [0.28261, 0.50666]
+        den = [1, -1.41833, 1.58939, -1.31608, 0.88642]
+        sys = scipysig.TransferFunction(num, den, dt=t_step)
+        u = np.random.normal(size=len(t))
+        t, y = scipysig.dlsim(sys, u, t)
+        data = iddata(y,u,t_step,[0,0,0])
+        r, _ = virtual_reference(data, num, den)
+        for i in range(len(r)):
+            print(" r[{}] = {} \t u[{}] = {}".format(i,r[i],i,u[i]))
+            self.assertTrue(np.isclose(r[i], u[i]))
+        
+
+
  
